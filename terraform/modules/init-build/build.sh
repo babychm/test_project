8a20a8ef0b78ebce6310bb47c6fd35d780f50454
Mykhailo Babych <fckurethn@gmail.com>
2022-06-20 14:25:47 +0300

    fixed init build

--- terraform/modules/init-build/build.sh
+++ terraform/modules/init-build/build.sh
@@ -4,6 +4,7 @@ git clone "$github_repo"
 cd  ./test_project/app
 sed -r "s/image:.*server.*/image: "$ecr_url":"$app_image_1"-"$app_tag"/g" docker-compose.yml > changed.yml && mv changed.yml docker-compose.yml
 sed -r "s/image:.*client.*/image: "$ecr_url":"$app_image_2"-"$app_tag"/g" docker-compose.yml > changed.yml && mv changed.yml docker-compose.yml
+docker-compose build
 docker-compose push
 cd ../..
 rm -rf test_project
