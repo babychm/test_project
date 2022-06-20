d0e63bc86b834136155db8620137e37f06ee85d2
Mykhailo Babych <fckurethn@gmail.com>
2022-06-20 13:43:32 +0300

    fixed init build

--- terraform/modules/init-build/build.sh
+++ terraform/modules/init-build/build.sh
@@ -2,8 +2,8 @@
 aws ecr get-login-password --region "$region" | docker login --username AWS --password-stdin "$registry_id".dkr.ecr."$region".amazonaws.com
 git clone "$github_repo"
 cd  ./test_project/app
-sed -r "s/image:.*server.*/image: $ecr_url:$app_image_1-$app_tag/g" docker-compose.yml > changed.yml && mv changed.yml docker-compose.yml
-sed -r "s/image:.*client.*/image: $ecr_url:$app_image_2-$app_tag/g" docker-compose.yml > changed.yml && mv changed.yml docker-compose.yml
+sed -r "s/image:.*server.*/image: "$ecr_url":"$app_image_1"-"$app_tag"/g" docker-compose.yml > changed.yml && mv changed.yml docker-compose.yml
+sed -r "s/image:.*client.*/image: "$ecr_url":"$app_image_2"-"$app_tag"/g" docker-compose.yml > changed.yml && mv changed.yml docker-compose.yml
 docker-compose push
 cd ../..
 rm -rf test_project
